4a373764cc3200170e81838d55c14d7065e23b36
AnkitaSMusale <ankitamusale4@gmail.com>
2022-05-19 16:21:27 +0530

    task4

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-console.dir(document);
+/*console.dir(document);
 console.log(document.domain);
 console.log(document.URL);
 console.log(document.title);
@@ -30,4 +30,19 @@ var heading= document.getElementById('heading');
 console.log(heading);
 heading.style.color='green';
 heading.innerHTML='<b>ADD ITEMS';
+*/
+
+//get element by classname
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[2]);
+items[2].textContent='Hello';
+items[2].style.fontWeight='bold';
+items[2].style.backgroundColor='green';
+//items.style.backgroundColor='pink'; //gives error ->hence use a for loop
+for(var i=0; i<items.length; i++)
+{
+    items[i].style.fontWeight='bold';
+    //items[i].style.color='red';
+}
 
