c12830c54e5498474613d76fa50f4b4df0be2658
AnkitaSMusale <ankitamusale4@gmail.com>
2022-06-02 11:44:16 +0530

    printing age - assign4

--- experimenting.js
+++ experimenting.js
@@ -1,5 +1,13 @@
-obj1 = {num:5}
+Student = {age : 20}
 
+function print(){
+    return this.age;
+}
+var bound = print.bind(Student);
+console.log(bound());
+
+
+/*obj1 = {num:5}
 function add(a , b , c){
     return this.num + a + b + c;
 }
@@ -9,4 +17,4 @@ function add(a , b , c){
 var arr = [4,6,10];
 //console.log(add.bind(obj1,arr));
 var bound = add.bind(obj1);
-console.log(bound(4,6,10));
+console.log(bound(4,6,10));*/
