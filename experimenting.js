e4616a940004bba0c8f3132641f211e88a02e0e5
AnkitaSMusale <ankitamusale4@gmail.com>
2022-06-02 12:06:44 +0530

    currying - assign4

--- experimenting.js
+++ experimenting.js
@@ -1,20 +1,27 @@
-Student = {age : 20}
-
-function print(){
-    return this.age;
-}
-var bound = print.bind(Student);
-console.log(bound());
-
-
-/*obj1 = {num:5}
-function add(a , b , c){
-    return this.num + a + b + c;
+/*  
+//one way to impelenting currying
+let multiply = function(x,y){
+    console.log(x*y);
 }
 
-//console.log(add.call(obj1,10));
-//console.log(add.apply(obj1,arr));
-var arr = [4,6,10];
-//console.log(add.bind(obj1,arr));
-var bound = add.bind(obj1);
-console.log(bound(4,6,10));*/
+//let multiplyby2 = multiply.bind(this , 2);  // by default set x = 2
+//multiplyby2(6);
+
+let multiplyby2 = multiply.bind(this , 2 , 5);  // by default set x = 2 & y =5 ,ignores 6
+multiplyby2(6);
+
+let multiplyby3 = multiply.bind(this , 3);   // by default set x = 3
+multiplyby3(6);
+*/
+
+//another way to impelenting currying
+let multiply = function(x){
+    return function(y){
+        console.log(x*y);
+    } 
+}
+let multiplyby2 = multiply(2);
+multiplyby2(6);
+
+let multiplyby3 = multiply(3);
+multiplyby3(6);
