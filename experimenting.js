c664794faea1e2ae5fd53884519e953adfe900f0
AnkitaSMusale <ankitamusale4@gmail.com>
2022-06-02 11:24:33 +0530

    Using apply - Assign4

--- experimenting.js
+++ experimenting.js
@@ -1,7 +1,9 @@
 obj1 = {num:5}
 
-function add(a){
-    return this.num + a ;
+function add(a , b , c){
+    return this.num + a + b + c;
 }
 
-console.log(add.call(obj1,10));
+//console.log(add.call(obj1,10));
+var arr = [4,6, 10];
+console.log(add.apply(obj1,arr));
