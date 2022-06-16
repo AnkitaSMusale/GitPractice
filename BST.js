65f796c13316f131a9a538fdc7435205a7873d58
AnkitaSMusale <ankitamusale4@gmail.com>
2022-06-16 21:31:18 +0530

    BST - Preorder Traversal .js

--- BST.js
+++ BST.js
@@ -1,4 +1,51 @@
-class Node{
+//Preorder Traversal
+class Node
+{
+    constructor(data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+ 
+class BinarySearch
+{
+    preorder(root){
+        if (root == null)
+        {
+            return;
+        }
+        console.log(root.data);
+        this.preorder(root.left);
+        this.preorder(root.right);
+    }
+}    
+ /* Construct the following tree
+                   15
+                 /   \
+                /     \
+               10       20
+              / \      /   \
+             /   \    /     \
+            8    14  18      22
+                   
+        */
+        const bst = new BinarySearch();
+        let root = new Node(15);
+        root.left = new Node(10);
+        root.right = new Node(20);
+        root.left.left = new Node(8);
+        root.left.right = new Node(14);
+        root.right.right = new Node(22);
+        root.right.left = new Node(18);
+        
+       // root.right.left.left = new Node(7);
+       // root.right.left.right = new Node(8);
+ 
+        bst.preorder(root);
+
+
+/*class Node{
     constructor(data){
         this.data = data;
         this.left = null;
@@ -67,3 +114,4 @@ var root = bst.getrootNode();
 console.log("Searching for an element :")
 console.log("Search for 10 :" ,bst.searching(10));
 console.log("Search for 180 :" ,bst.searching(180));
+*/
