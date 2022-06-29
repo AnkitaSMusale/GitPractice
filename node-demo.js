91ec2301d9671ee2020e9fedff2b6f8ce89b3422
AnkitaSMusale <ankitamusale4@gmail.com>
2022-06-29 16:18:40 +0530

    Request/Response using node .js

--- node-demo.js
+++ node-demo.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const http = require('http');
+const server = http.createServer(function (req,res){
+    console.log(req);
+    console.log('Ankita');
+
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>');
+    res.write('<head><title>Node js Server</title></html>');
+    res.write('<body>Hello from my Node JS Server</body>');
+    res.write('</html>');
+    res.end();
+})
+server.listen(4000);
+
+
