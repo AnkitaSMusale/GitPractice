c86dab46812bf81db74d0776c74101a8dbe04c21
AnkitaSMusale <ankitamusale4@gmail.com>
2022-06-29 16:55:45 +0530

    Printing name On Browser Using Node .js

--- node-demo.js
+++ node-demo.js
@@ -2,14 +2,9 @@ const http = require('http');
 const server = http.createServer(function (req,res){
     console.log(req);
     console.log('Ankita');
-
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<html>');
-    res.write('<head><title>Node js Server</title></html>');
-    res.write('<body>Hello from my Node JS Server</body>');
-    res.write('</html>');
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('Ankita');
     res.end();
 })
 server.listen(4000);
-
 
