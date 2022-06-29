963f47bc918e660f8cee71fa5595b5902ae2ae00
AnkitaSMusale <ankitamusale4@gmail.com>
2022-06-29 17:47:04 +0530

    Hitting the url with Node JS

--- node-demo.js
+++ node-demo.js
@@ -1,10 +1,39 @@
+
 const http = require('http');
 const server = http.createServer(function (req,res){
-    console.log(req);
+    console.log(req.url ,req.method);
     console.log('Ankita');
-    res.setHeader('Content-Type', 'text/plain')
-    res.write('Ankita');
-    res.end();
+    res.setHeader('Content-Type', 'text/html')
+    if(req.url === '/home'){
+        res.write('<html>');
+        res.write('<head><title>Node js Server</title></html>');
+        res.write('<body>Welcome home</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(req.url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>Node js Server</title></html>');
+        res.write('<body>Welcome to About Us page</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else if(req.url === '/node') {
+        res.write('<html>');
+        res.write('<head><title>Node js Server</title></html>');
+        res.write('<body>Welcome to my Node JS project</body>');
+        res.write('</html>');
+        return res.end();
+    }
+    else{
+        res.write('<html>');
+        res.write('<head><title>Node js Server</title></html>');
+        res.write('<body>Hello</body>');
+        res.write('</html>');
+        return res.end();
+    }
 })
 server.listen(4000);
+
+
 
