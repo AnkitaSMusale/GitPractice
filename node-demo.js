c9cee916c67df158d09454aa2030904de8d9c268
AnkitaSMusale <ankitamusale4@gmail.com>
2022-06-30 19:03:42 +0530

    Module Exporting through routing .js

--- node-demo.js
+++ node-demo.js
@@ -1,39 +1,11 @@
-
 const http = require('http');
-const server = http.createServer(function (req,res){
-    console.log(req.url ,req.method);
-    console.log('Ankita');
-    res.setHeader('Content-Type', 'text/html')
-    if(req.url === '/home'){
-        res.write('<html>');
-        res.write('<head><title>Node js Server</title></html>');
-        res.write('<body>Welcome home</body>');
-        res.write('</html>');
-        return res.end();
-    }
-    else if(req.url === '/about') {
-        res.write('<html>');
-        res.write('<head><title>Node js Server</title></html>');
-        res.write('<body>Welcome to About Us page</body>');
-        res.write('</html>');
-        return res.end();
-    }
-    else if(req.url === '/node') {
-        res.write('<html>');
-        res.write('<head><title>Node js Server</title></html>');
-        res.write('<body>Welcome to my Node JS project</body>');
-        res.write('</html>');
-        return res.end();
-    }
-    else{
-        res.write('<html>');
-        res.write('<head><title>Node js Server</title></html>');
-        res.write('<body>Hello</body>');
-        res.write('</html>');
-        return res.end();
-    }
-})
-server.listen(4000);
+const routes = require('./routes');
 
+//syntax1
+const server = http.createServer(routes);
 
+//syntax2/3/4
+//console.log(routes.someText);
+//const server = http.createServer(routes.handler); 
 
+server.listen(3000);
