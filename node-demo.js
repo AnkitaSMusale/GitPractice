e533d0e50af5a79cbf95c9222bd2bec4f9c6935d
AnkitaSMusale <ankitamusale4@gmail.com>
2022-07-01 12:50:44 +0530

    Redirecting with 302 response .js

--- node-demo.js
+++ node-demo.js
@@ -1,11 +1,30 @@
 const http = require('http');
-const routes = require('./routes');
-
-//syntax1
-const server = http.createServer(routes);
-
-//syntax2/3/4
-//console.log(routes.someText);
-//const server = http.createServer(routes.handler); 
-
+const fs = require('fs');
+const server = http.createServer(function (req,res){
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>Enter message</title></html>');
+        res.write('<body><form action = "/message" method = "POST"><input type ="text" name="message"><button type="submit">Send</button></body>');
+        res.write('</html>');
+        return res.end()
+    }
+    if(url === '/message' && method==='POST'){
+        fs.writeFileSync('message.txt','Dummy')
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+      
+        };
+    
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>');
+    res.write('<head><title>Node js Server</title></html>');
+    res.write('<body>Welcome to my Node JS project</body>');
+    res.write('</html>');
+    res.end();
+})
 server.listen(3000);
+
+
