54c0e04d4bdbc5c23d1b1ae830de97f5a8f37c98
AnkitaSMusale <ankitamusale4@gmail.com>
2022-07-04 23:56:45 +0530

    Middleware - add product page

--- node-demo.js
+++ node-demo.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
+const bodyparser = require('body-parser');
+const { urlencoded } = require('body-parser');
 
-app.use((req,res,next)=>{
-    console.log('In the middleware')
-    next();
+app.use(bodyparser.urlencoded({extended : false}));
+
+app.use('/addproduct',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"> <input type="text" name="quantity" placeholder="Add quantity here..."><button type="Submit">Add product</button></form>')
 })
 
-app.use((req,res,next)=>{
-    console.log('In another middleware')
-    res.send('<h1>Hello From Express JS</h1>')
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req,res,next)=>{
+    res.send( '<h1>Hello from Express JS</h1>')
 })
 app.listen(3000);
 
